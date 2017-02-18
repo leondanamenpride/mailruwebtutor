1a15a807b99f708731e2c48cbdddeae45923011f
leondanamenpride <leondanamenpride@yandex.ru>
2017-02-18 17:38:59 +0500

    global changes2

--- init.sh
+++ init.sh
@@ -3,5 +3,4 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo gunicorn - b 0.0.0.0:8080 hello:app
 sudo /etc/init.d/mysql start
