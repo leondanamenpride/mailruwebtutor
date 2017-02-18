866ad968bfd8b205d2c41324eae2512305554c40
leondanamenpride <leondanamenpride@yandex.ru>
2017-02-18 16:51:03 +0500

    change hello.py[2]

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
- sudo gunicorn -b 0.0.0.0:8080 hello:app &
+sudo gunicorn -b 0.0.0.0:8080 hello:app
 sudo /etc/init.d/mysql start
