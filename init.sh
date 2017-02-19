41dce75d6000105d461ba9342e3854dcc7567e76
leondanamenpride <leondanamenpride@yandex.ru>
2017-02-19 20:43:38 +0500

    change init.sh file

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo ln -s ~/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
