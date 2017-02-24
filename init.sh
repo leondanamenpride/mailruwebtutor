295700a6cae85f82f2d89a4d61a6156c79c70819
leondanamenpride <leondanamenpride@yandex.ru>
2017-02-25 01:29:11 +0500

    add manage py files and change server sh

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf ~/web/etc/guni.py   /etc/gunicorn.d/guni.py
+sudo ln -sf ~/web/etc/guni.py   /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
