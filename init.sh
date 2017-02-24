aa4df79f06c6a17f1544b2f8f7e47876615b2063
leondanamenpride <leondanamenpride@yandex.ru>
2017-02-24 23:24:43 +0500

    change gunicorn.config[1}

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s ~/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn -b 0.0.0.0:8080 hello:app &
+sudo /etc/init.d/gunicorn -w 1 -b 0.0.0.0:8000 ask.wsgi
 sudo /etc/init.d/mysql start
